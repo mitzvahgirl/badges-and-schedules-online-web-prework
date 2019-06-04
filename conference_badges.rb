9c107dba4c4f353d58e5a845db6070de4ea87d30
Amy Fogelman <abfnj1@gmail.com>
2019-06-04 13:06:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr)
-  
+ new_array = []
+  arr.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end 
   
 def assign_rooms(arr) 
   
