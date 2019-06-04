1f3b04ab0ed365dbae6043103e9c12b11607a4d8
Amy Fogelman <abfnj1@gmail.com>
2019-06-04 12:59:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,11 @@ def batch_badge_creator(arr)
 def assign_rooms(arr) 
   
   "Hello, #{name}! You'll be assigned to room {counter}!"
-  
-  def printer 
-    batch_badge_creator
-    
-    assign_rooms
+ def printer(arr)
+  batch_badge_creator(arr).each do |badge|
+    puts badge
+  end
+
+   assign_rooms(arr).each do |assignment|
+    puts assignment
+  end
